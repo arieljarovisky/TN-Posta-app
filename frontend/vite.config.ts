6d52b3db8284f7abb3d83e52379a36789247045f
Ariel <jaroviskyariel@gmail.com>
2026-06-28 01:51:32 -0300

    Update dependencies for DnD Kit and enhance Vite configuration
    - Added @dnd-kit/core, @dnd-kit/sortable, and @dnd-kit/utilities to package.json for improved drag-and-drop functionality.
    - Updated yarn.lock to reflect new versions of DnD Kit packages.
    - Enhanced Vite configuration to include DnD Kit dependencies in the optimization process and adjusted build options for better compatibility.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,10 +6,18 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 export default defineConfig({
   optimizeDeps: {
     include: [
+      "@dnd-kit/core",
+      "@dnd-kit/sortable",
+      "@dnd-kit/utilities",
       "@nimbus-ds/patterns",
       "@nimbus-ds/components",
       "@nimbus-ds/styles",
     ],
+  },
+  build: {
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
   },
   plugins: [
     react(),
@@ -19,6 +27,7 @@ export default defineConfig({
         global: true,
         process: true,
       },
+      protocolImports: false,
     }),
   ],
   resolve: {
